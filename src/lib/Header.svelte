f12bf0e31bcbef2bbf408f5f1af1150ccf34888d
Frederick O'Brien <frederik.obrien@gmail.com>
2021-08-19 00:42:01 +0100

    Laying foundations
    Header, layout, and error components, filler homepage. Replicating what was previously in the newsletter repo, but tidier.

---
+++ src/lib/Header.svelte
@@ -0,0 +1,108 @@
+<script>
+
+</script>
+
+<header class="navbar">
+    <a href="/" class="audioxide-archive-logo"><span class="logo">Audioxide</span> Archive</a>
+    <a href="https://audioxide.eo.page/newsletter"><span class="subscription-prompt">Subscribe for monthly roundups</span> →</a>
+    <nav class="dropdown">
+        <button class="dropbtn">Browse editions
+            <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+            <a href="/newsletters/2021-07/">July 02021</a>
+            <a href="/newsletters/2021-06/">June 02021</a>
+            <a href="/newsletters/2021-05/">May 02021</a>
+            <a href="/newsletters/2021-04/">April 02021</a>
+            <a href="/newsletters/2021-03/">March 02021</a>
+            <a href="/newsletters/2021-02/">February 02021</a>
+            <a href="/newsletters/2021-01/">January 02021</a>
+            <a href="/newsletters/2021-02/">December 02021</a>
+            <a href="/newsletters/2021-01/">November 02021</a>
+        </div>
+    </nav>
+</header>
+
+<style>
+
+    .logo {
+        font-family: 'Rounded Elegance', sans-serif;
+        font-weight: 400;
+        text-transform: lowercase;
+    }
+
+    .audioxide-archive-logo {
+        border-right: 2px solid #f5f5f5;
+        font-weight: 700;
+    }
+
+	.navbar {
+		overflow: hidden;
+		background-color: #222;
+		font-family: 'Source Sans Pro', sans-serif;
+		box-shadow: 0 3px 6px 0 rgb(0 0 0 / 30%);
+		z-index: 1;
+	}
+
+    .navbar a {
+		float: left;
+		font-size: 20px;
+		color: white;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+	}
+
+    .dropdown {
+		float: right;
+		overflow: hidden;
+	}
+
+    .dropdown .dropbtn {
+		font-size: 20px;
+		border: none;
+		outline: none;
+		color: white;
+		padding: 14px 16px;
+		background-color: inherit;
+		font-family: inherit;
+		margin: 0;
+	}
+
+	.navbar a:hover, .dropdown:hover .dropbtn {
+		background-color: #f5f5f5;
+		color: #282828;
+	}
+
+    .dropdown-content {
+		display: none;
+		position: absolute;
+		background-color: #f9f9f9;
+		width: 165px;
+		box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+		z-index: 1;
+	}
+
+    .dropdown-content a {
+		font-size: 16px;
+		float: none;
+		color: black;
+		padding: 12px 16px;
+		text-decoration: none;
+		display: block;
+		text-align: left;
+	}
+
+    .dropdown-content a:hover {
+		background-color: #ddd;
+	}
+
+    .dropdown:hover .dropdown-content {
+		display: block;
+	}
+
+    .subscription-prompt {
+        text-decoration: underline;
+    }
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Audioxide Archive</h1>
-<p>Coming soon.</p>
+<svelte:head>
+    <title>Audioxide Archive</title>
+</svelte:head>
+
+<div class="teaser-message">
+    <h2>Coming soon.</h2>
+</div>
+
+<style>
+    .teaser-message {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+    }
+</style>
