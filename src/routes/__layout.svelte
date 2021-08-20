2f971364f0b2300d0750355f2eb0d088f4c0907e
Frederick O'Brien <frederick.obrien@guardian.co.uk>
2021-08-20 21:16:37 +0100

    Move font imports

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,8 @@
+<svelte:head>
+	<link href="http://fonts.cdnfonts.com/css/rounded-elegance" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+</svelte:head>
+
 <script>
 	import Header from '../lib/Header.svelte';
 	import '../app.css';
