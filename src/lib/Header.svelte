d3dbbe2e8ef6ed236127ce6abf4381338cf7d5d3
Frederick O'Brien <frederik.obrien@gmail.com>
2021-11-28 21:30:35 +0000

    October 2021

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,118 +2,118 @@
 </script>
 
 <header class="navbar">
-	<a href="/" class="audioxide-archive-logo hide-on-mobile"
-		><span class="logo">Audioxide</span> Archive</a
-	>
-	<a href="https://audioxide.eo.page/newsletter"
-		><span class="subscription-prompt"
-			>Subscribe <span class="hide-on-mobile">for monthly roundups</span
-			></span
-		> →</a
-	>
-	<nav class="dropdown">
-		<button class="dropbtn"
-			>Browse editions
-			<i class="fa fa-caret-down" />
-		</button>
-		<div class="dropdown-content">
-			<a href="/2021-09">September 02021</a>
-			<a href="/2021-08">August 02021</a>
-			<a href="/2021-07">July 02021</a>
-			<a href="/2021-06">June 02021</a>
-			<a href="/2021-05">May 02021</a>
-			<!--            <a href="/2021-04">April 02021</a>-->
-			<!--            <a href="/2021-03">March 02021</a>-->
-			<!--            <a href="/2021-02">February 02021</a>-->
-			<!--            <a href="/2021-01">January 02021</a>-->
-			<!--            <a href="/2020-12">December 02021</a>-->
-			<!--            <a href="/2020-11">November 02021</a>-->
-		</div>
-	</nav>
+  <a href="/" class="audioxide-archive-logo hide-on-mobile"
+    ><span class="logo">Audioxide</span> Archive</a
+  >
+  <a href="https://audioxide.eo.page/newsletter"
+    ><span class="subscription-prompt"
+      >Subscribe <span class="hide-on-mobile">for monthly roundups</span></span
+    > →</a
+  >
+  <nav class="dropdown">
+    <button class="dropbtn"
+      >Browse ‘editions’
+      <i class="fa fa-caret-down" />
+    </button>
+    <div class="dropdown-content">
+      <a href="/2021-10">October 02021</a>
+      <a href="/2021-09">September 02021</a>
+      <a href="/2021-08">August 02021</a>
+      <a href="/2021-07">July 02021</a>
+      <a href="/2021-06">June 02021</a>
+      <a href="/2021-05">May 02021</a>
+      <!--            <a href="/2021-04">April 02021</a>-->
+      <!--            <a href="/2021-03">March 02021</a>-->
+      <!--            <a href="/2021-02">February 02021</a>-->
+      <!--            <a href="/2021-01">January 02021</a>-->
+      <!--            <a href="/2020-12">December 02021</a>-->
+      <!--            <a href="/2020-11">November 02021</a>-->
+    </div>
+  </nav>
 </header>
 
 <style>
-	.audioxide-archive-logo {
-		border-right: 2px solid #f5f5f5;
-		font-weight: 700;
-	}
+  .audioxide-archive-logo {
+    border-right: 2px solid #f5f5f5;
+    font-weight: 700;
+  }
 
-	.navbar {
-		background-color: #222;
-		font-family: "Source Sans Pro", sans-serif;
-		box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.3);
-		position: fixed;
-  top: 0;
-  width: 100%;
-		z-index: 1;
-	}
+  .navbar {
+    background-color: #222;
+    font-family: "Source Sans Pro", sans-serif;
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.3);
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: 1;
+  }
 
-	.navbar a {
-		float: left;
-		font-size: 20px;
-		color: white;
-		text-align: center;
-		padding: 14px 16px;
-		text-decoration: none;
-	}
+  .navbar a {
+    float: left;
+    font-size: 20px;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
 
-	.dropdown {
-		float: right;
-		overflow: hidden;
-	}
+  .dropdown {
+    float: right;
+    overflow: hidden;
+  }
 
-	.dropdown .dropbtn {
-		font-size: 20px;
-		border: none;
-		outline: none;
-		color: white;
-		padding: 14px 16px;
-		background-color: inherit;
-		font-family: inherit;
-		margin: 0;
-	}
+  .dropdown .dropbtn {
+    font-size: 20px;
+    border: none;
+    outline: none;
+    color: white;
+    padding: 14px 16px;
+    background-color: inherit;
+    font-family: inherit;
+    margin: 0;
+  }
 
-	.navbar a:hover,
-	.dropdown:hover .dropbtn {
-		background-color: #f5f5f5;
-		color: #282828;
-	}
+  .navbar a:hover,
+  .dropdown:hover .dropbtn {
+    background-color: #f5f5f5;
+    color: #282828;
+  }
 
-	.dropdown-content {
-		display: none;
-		position: absolute;
-		background-color: #f9f9f9;
-		width: 165px;
-		box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-		z-index: 1;
-	}
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    width: 165px;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    z-index: 1;
+  }
 
-	.dropdown-content a {
-		font-size: 16px;
-		float: none;
-		color: black;
-		padding: 12px 16px;
-		text-decoration: none;
-		display: block;
-		text-align: left;
-	}
+  .dropdown-content a {
+    font-size: 16px;
+    float: none;
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+    text-align: left;
+  }
 
-	.dropdown-content a:hover {
-		background-color: #ddd;
-	}
+  .dropdown-content a:hover {
+    background-color: #ddd;
+  }
 
-	.dropdown:hover .dropdown-content {
-		display: block;
-	}
+  .dropdown:hover .dropdown-content {
+    display: block;
+  }
 
-	.subscription-prompt {
-		text-decoration: underline;
-	}
+  .subscription-prompt {
+    text-decoration: underline;
+  }
 
-	@media screen and (max-width: 600px) {
-		.hide-on-mobile {
-			visibility: hidden;
-			display: none;
-		}
-	}
+  @media screen and (max-width: 600px) {
+    .hide-on-mobile {
+      visibility: hidden;
+      display: none;
+    }
+  }
 </style>
---
+++ src/routes/2021-10/index.svelte
@@ -0,0 +1,617 @@
+<svelte:head>
+  <title>Audioxide Archive: October 02021</title>
+</svelte:head>
+
+<!-- 100% background wrapper (grey background) -->
+<table
+  border="0"
+  width="100%"
+  height="100%"
+  cellpadding="0"
+  cellspacing="0"
+  bgcolor="#fff"
+>
+  <tr>
+    <td
+      align="center"
+      valign="top"
+      bgcolor="#fff"
+      style="background-color: #ffffff;"
+    >
+      <!-- 600px container (white background) -->
+
+      <table
+        border="0"
+        width="600"
+        cellpadding="0"
+        cellspacing="0"
+        class="container container-padding"
+      >
+        <!-- INTRODUCTION -->
+
+        <tr>
+          <td class="newsletter-section">
+            <table>
+              <tr>
+                <td
+                  style="background-image: url('images/leads/2021-10-james-blake-by-amanda-charchian.jpg')"
+                  class="lead-image"
+                >
+                  <a
+                    class="lead-image-credit"
+                    href="https://chuffmedia.com/artists/james-blake"
+                    >Photograph by Amanda Charchian →</a
+                  >
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <img
+                    src="https://raw.githubusercontent.com/audioxide/brand/main/assets/png/bordered-emblem-black-and-white.png"
+                    class="audioxide-emblem"
+                    alt="Black and white Audioxide emblem"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p class="month">October 02021</p>
+                  <h2 class="title">Artists That Break Your Heart</h2>
+                  <p>Oh Jimmy. Sweet, predictable Jimmy.</p>
+                  <p>
+                    Electronic comfort zone extraordinaire James Blake returned
+                    this month with a new album called <i
+                      ><strong>Friends That Break Your Heart</strong></i
+                    >. After signs of evolution in his previous record it was
+                    disappointing to hear him fall back into his old ways,
+                    though still very mind. We followed that up with
+                    <i><strong>Skin</strong></i>, the debut record of one Joy
+                    Crookes. Bloody fabulous, a kind of big band pop. We cannot
+                    wait to hear where she goes next.
+                  </p>
+                  <p>
+                    Modest progress made on the album artwork stories, with only
+                    four more added to the database. Still, four more than zero,
+                    right? One day. One day.
+                  </p>
+                  <p>Until next time,</p>
+                  <p>André, Fred, and Andrew</p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <!-- REVIEWS -->
+
+        <tr>
+          <td class="newsletter-section">
+            <table>
+              <tr>
+                <td>
+                  <h3 class="section-heading">
+                    <span class="section-heading-wrapper"> Reviews </span>
+                  </h3>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <!--[if mso]>
+                                    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                        <tr>
+                                            <td width="50%" valign="top"><![endif]-->
+
+                  <table
+                    width="215"
+                    border="0"
+                    cellpadding="0"
+                    cellspacing="0"
+                    align="left"
+                    class="force-row"
+                  >
+                    <tr>
+                      <td class="col" valign="top">
+                        <img
+                          src="https://audioxide.com/api/images/album-artwork/friends-that-break-your-heart-james-blake-medium-square.jpg"
+                          alt="Album artwork of 'Friends That Break Your Heart' by James Blake"
+                          class="review-image"
+                          style="border: 3px solid #5b9e18;"
+                        />
+                      </td>
+                    </tr>
+                  </table>
+
+                  <!--[if mso]></td>
+                                <td width="50%" valign="top"><![endif]-->
+
+                  <table
+                    width="355"
+                    border="0"
+                    cellpadding="0"
+                    cellspacing="0"
+                    align="right"
+                    class="force-row"
+                  >
+                    <tr>
+                      <td class="col review-info" valign="top">
+                        <h4 class="review-album-name" style="color: #5b9e18;">
+                          Friends That Break Your Heart
+                        </h4>
+                        <h4 class="review-artist-name">James Blake</h4>
+                        <p class="review-summary">
+                          ‘Blake’s vocals are, predictably, beautiful; Blake’s
+                          production is, predictably, meticulous; and I,
+                          predictably, have lapped it up. But where Assume Form
+                          felt like progress, Friends That Break Your Heart
+                          feels a little too familiar.’
+                        </p>
+                        <span
+                          ><a
+                            href="https://audioxide.com/reviews/james-blake-friends-that-break-your-heart/"
+                            >Read review</a
+                          > →</span
+                        >
+                      </td>
+                    </tr>
+                  </table>
+
+                  <!--[if mso]></td></tr></table><![endif]-->
+
+                  <div class="review-divider" />
+                  <hr class="hr" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <!--[if mso]>
+                                    <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                        <tr>
+                                            <td width="50%" valign="top"><![endif]-->
+
+                  <table
+                    width="215"
+                    border="0"
+                    cellpadding="0"
+                    cellspacing="0"
+                    align="left"
+                    class="force-row"
+                  >
+                    <tr>
+                      <td class="col" valign="top">
+                        <img
+                          src="https://audioxide.com/api/images/album-artwork/skin-joy-crookes-medium-square.jpg"
+                          alt="Album artwork of 'Skin' by Joy Crookes"
+                          class="review-image"
+                          style="border: 3px solid #876a64;"
+                        />
+                      </td>
+                    </tr>
+                  </table>
+
+                  <!--[if mso]></td>
+                                <td width="50%" valign="top"><![endif]-->
+
+                  <table
+                    width="355"
+                    border="0"
+                    cellpadding="0"
+                    cellspacing="0"
+                    align="right"
+                    class="force-row"
+                  >
+                    <tr>
+                      <td class="col review-info" valign="top">
+                        <h4 class="review-album-name" style="color: #876a64;">
+                          Skin
+                        </h4>
+                        <h4 class="review-artist-name">Joy Crookes</h4>
+                        <p class="review-summary">
+                          ‘Crookes has given me another reason to think 2021 is
+                          the year of the confident, brilliant debut. Skin has a
+                          knack for swaying between intimate, soulful jazz cuts
+                          and grander, orchestral pieces, making for a
+                          satisfying, moreish sound.’
+                        </p>
+                        <span
+                          ><a
+                            href="https://audioxide.com/reviews/joy-crookes-skin/"
+                            >Read review</a
+                          > →</span
+                        >
+                      </td>
+                    </tr>
+                  </table>
+
+                  <!--[if mso]></td></tr></table><![endif]-->
+
+                  <div class="review-divider" />
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+
+        <!-- ARTWORK STORIES -->
+
+        <tr>
+          <td class="newsletter-section">
+            <table>
+              <tr>
+                <td>
+                  <h3 class="section-heading">
+                    <span class="section-heading-wrapper">
+                      Artwork stories
+                    </span>
+                  </h3>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p>
+                    We only managed to add a handful of cover credits this
+                    month, but it was a lovely handful. Below were the best of
+                    them.
+                  </p>
+                  <table class="artwork-story-grid-container">
+                    <tr>
+                      <td>
+                        <a
+                          href="https://audioxide.com/reviews/pink-floyd-the-dark-side-of-the-moon/"
+                        >
+                          <img
+                            src="https://audioxide.com/api/images/album-artwork/the-dark-side-of-the-moon-pink-floyd-medium-square.jpg"
+                            class="artwork-story-image"
+                            alt="Album artwork of 'The Dark Side of the Moon' by Pink Floyd"
+                            style="border: 3px solid #000;"
+                          />
+                        </a>
+                      </td>
+                      <td>
+                        <a
+                          href="https://audioxide.com/reviews/thom-yorke-the-eraser/"
+                        >
+                          <img
+                            src="https://audioxide.com/api/images/album-artwork/the-eraser-thom-yorke-medium-square.jpg"
+                            class="artwork-story-image"
+                            alt="Album artwork of 'The Eraser' by Thom Yorke"
+                            style="border: 3px solid #000;
+                                                            margin: auto;
+                                                            display: block;"
+                          />
+                        </a>
+                      </td>
+                      <td>
+                        <a
+                          href="https://audioxide.com/reviews/new-order-power-corruption-and-lies/"
+                        >
+                          <img
+                            src="https://audioxide.com/api/images/album-artwork/power-corruption-and-lies-new-order-medium-square.jpg"
+                            class="artwork-story-image"
+                            alt="Album artwork of 'Power, Corruption & Lies' by New Order"
+                            style="border: 3px solid #3d3246;
+                                                            float: right;"
+                          />
+                        </a>
+                      </td>
+                    </tr>
+                  </table>
+                  <p>
+                    It was especially interesting to learn that the cover of <i
+                      >The Eraser</i
+                    >
+                    by Thom Yorke - designed by longtime Radiohead collaborator Stanley
+                    Donwood - was but a fraction of a massive artwork depicting disaster
+                    of biblical proportions. You can see the full list of credits
+                    added this month in
+                    <a href="https://github.com/audioxide/data/pull/86"
+                      >the GitHub pull request.</a
+                    >.
+                  </p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table></td
+    >
+  </tr>
+</table>
+<!--/600px container -->
+
+<!--/100% background wrapper-->
+<style>
+  /* FOUNDATIONS */
+
+  @import url("https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Spectral:ital,wght@0,200;0,300;0,400;0,500;0,600;1,200;1,300;1,400;1,500;1,600&display=swap");
+  @import url("http://fonts.cdnfonts.com/css/rounded-elegance");
+
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: "Source Sans Pro", sans-serif;
+    font-size: 18px;
+    margin: 0;
+    padding: 0;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+  }
+
+  p,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin-bottom: 15px;
+    line-height: 150%;
+  }
+
+  h4 {
+    font-size: 22px;
+  }
+
+  img {
+    width: 100%;
+  }
+  a {
+    color: #f01d4f;
+  }
+
+  a:hover {
+    color: pink;
+  }
+
+  .container {
+    width: 600px;
+    max-width: 600px;
+    margin: 0;
+    padding: 0;
+  }
+
+  /* LAYOUT */
+
+  .section-heading,
+  .title,
+  .month {
+    text-align: center;
+  }
+
+  .section-heading {
+    color: #f5f5f5;
+    margin-bottom: 60px;
+    font-family: "Rounded Elegance", sans-serif;
+    font-weight: 600;
+    letter-spacing: 5px;
+    text-transform: lowercase;
+  }
+
+  .section-heading-wrapper {
+    background-color: #000;
+    font-family: "Rounded Elegance", sans-serif;
+    filter: drop-shadow(0 0 0.3rem rgb(180, 180, 180));
+    padding: 20px 40px;
+    border-radius: 30px;
+  }
+
+  .newsletter-section {
+    padding-bottom: 90px;
+  }
+
+  .container-padding {
+    padding-left: 12px;
+    padding-right: 12px;
+  }
+
+  hr {
+    border: 0;
+    border-bottom: 1px solid #cccccc;
+  }
+
+  .hr {
+    height: 1px;
+    border-bottom: 3px solid #f5f5f5;
+    margin: 35px 0;
+  }
+
+  /* INTRODUCTION */
+
+  .lead-image {
+    height: 300px;
+    border-bottom-right-radius: 10px;
+    border-bottom-left-radius: 10px;
+    background-size: cover;
+    position: relative;
+  }
+
+  .lead-image-credit {
+    position: absolute;
+    top: 10px;
+    right: 15px;
+    font-size: 12px;
+    color: #f5f5f5;
+    padding: 4px 10px;
+    border-radius: 10px;
+    text-decoration: none;
+    /*background-color: lightgray;*/
+    opacity: 0.8;
+  }
+
+  .lead-image-credit:hover {
+    color: white;
+    text-decoration: underline;
+  }
+
+  .audioxide-emblem {
+    width: 15%;
+    margin: -50px auto 15px;
+    display: block;
+    filter: drop-shadow(0 0 0.6rem rgb(90, 90, 90));
+  }
+
+  .month {
+    margin-bottom: 0px;
+    font-size: 16px;
+    font-family: "Rounded Elegance", sans-serif;
+  }
+
+  /* REVIEWS */
+
+  .review-image {
+    border-radius: 5px;
+  }
+
+  .review-info {
+    padding-left: 30px;
+  }
+
+  .review-album-name {
+    font-style: italic;
+    margin-bottom: 0px;
+  }
+
+  .review-artist-name {
+    font-size: 20px;
+  }
+
+  .review-summary {
+    font-family: "Spectral", serif;
+    font-style: italic;
+  }
+
+  .review-divider {
+    clear: both;
+  }
+
+  /* ARTICLES */
+
+  .article-image {
+    border-radius: 5px;
+    margin-bottom: 30px;
+  }
+
+  /* INTERVIEWS */
+
+  .interview-pullquote {
+    font-style: italic;
+    font-family: "Spectral", serif;
+  }
+
+  .interview-image {
+    border-radius: 5px;
+  }
+
+  /* INTERVIEWS */
+
+  .funnyfarm-image {
+    border-radius: 5px;
+  }
+
+  /* ARTWORK STORIES */
+
+  .artwork-story-grid-container {
+    padding-bottom: 15px;
+  }
+
+  .artwork-story-image {
+    border-radius: 5px;
+    width: 95%;
+  }
+
+  .artwork-story-image:hover {
+    opacity: 0.7;
+  }
+
+  /* SITE DEVELOPMENT */
+
+  .site-dev-image {
+    margin-bottom: 15px;
+  }
+
+  /* FOOTER */
+
+  .footer-text {
+    font-size: 12px;
+    line-height: 16px;
+    color: #aaaaaa;
+  }
+  .footer-text a {
+    color: #aaaaaa;
+  }
+
+  /* TBD */
+
+  a[href^="x-apple-data-detectors:"],
+  a[x-apple-data-detectors] {
+    color: inherit !important;
+    text-decoration: none !important;
+    font-size: inherit !important;
+    font-family: inherit !important;
+    font-weight: inherit !important;
+    line-height: inherit !important;
+  }
+
+  table {
+    border-spacing: 0;
+  }
+
+  table td {
+    border-collapse: collapse;
+  }
+
+  .ExternalClass {
+    width: 100%;
+  }
+
+  .ExternalClass,
+  .ExternalClass p,
+  .ExternalClass span,
+  .ExternalClass font,
+  .ExternalClass td,
+  .ExternalClass div {
+    line-height: 100%;
+  }
+
+  .ReadMsgBody {
+    width: 100%;
+    background-color: #ebebeb;
+  }
+
+  table {
+    mso-table-lspace: 0pt;
+    mso-table-rspace: 0pt;
+  }
+
+  img {
+    -ms-interpolation-mode: bicubic;
+  }
+
+  .yshortcuts a {
+    border-bottom: none !important;
+  }
+
+  @media screen and (max-width: 599px) {
+    .force-row,
+    .container {
+      width: 100% !important;
+      max-width: 100% !important;
+    }
+    .review-info {
+      padding-left: 0;
+      padding-top: 20px;
+    }
+  }
+  @media screen and (max-width: 400px) {
+    .container-padding {
+      padding-left: 12px !important;
+      padding-right: 12px !important;
+    }
+  }
+  .ios-footer a {
+    color: #aaaaaa !important;
+    text-decoration: underline;
+  }
+</style>
